47dc24f9e78a27c02f50026cae2609987ac054c8
mukulasija <lit2020010@iiitl.ac.in>
2024-01-11 23:05:24 +0530

    added splash ui and auth ui

---
+++ app/src/main/java/com/example/picprints/ui/screens/splash/SplashActivity.kt
@@ -0,0 +1,16 @@
+package com.example.picprints.ui.screens.splash
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.picprints.R
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+
+    fun navigateToAuthScreen() {
+        
+    }
+}
