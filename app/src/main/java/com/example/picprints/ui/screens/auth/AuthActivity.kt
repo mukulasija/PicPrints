cf5088c7ef86280f08b4e288931864cd98d615b0
mukulasija <lit2020010@iiitl.ac.in>
2024-01-12 22:48:53 +0530

    created auth fragments

--- app/src/main/java/com/example/picprints/ui/screens/auth/AuthActivity.kt
+++ app/src/main/java/com/example/picprints/ui/screens/auth/AuthActivity.kt
@@ -2,11 +2,18 @@ package com.example.picprints.ui.screens.auth
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
 import com.example.picprints.R
+import com.example.picprints.ui.screens.auth.gettingStarted.BlankFragment
+import com.example.picprints.ui.screens.auth.gettingStarted.GettingStartedFragment
+import com.example.picprints.ui.screens.auth.login.LoginFragment
 
 class AuthActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_auth)
+
     }
 }
--- app/src/main/java/com/example/picprints/ui/screens/splash/SplashActivity.kt
+++ app/src/main/java/com/example/picprints/ui/screens/splash/SplashActivity.kt
@@ -1,16 +1,27 @@
 package com.example.picprints.ui.screens.splash
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import com.example.picprints.MainActivity
 import com.example.picprints.R
+import com.example.picprints.ui.screens.auth.AuthActivity
 
 class SplashActivity : AppCompatActivity() {
+    private val SPLASH_DELAY: Long = 3000 // 2 seconds delay
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+        Handler().postDelayed({
+            navigateToAuthScreen()
+        }, SPLASH_DELAY)
     }
 
     fun navigateToAuthScreen() {
-        
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+        finish() // Optional: finish the current activity so that the user can't go back to the splash screen
     }
 }
